8cf0cbb2ccc0533d55ee7e55a9540d7506295daf
miniwei101 <miniwei101@yahoo.com>
2022-03-05 16:58:06 +0800

    googleLogin

---
+++ Auth/Auth/Models/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Auth.Models
+{
+    public class Class1
+    {
+        public int cId { get; set; }
+        public string c_name { get; set; }
+        public string c_address { get; set; }
+        public string c_account { get; set; }
+        public string c_password { get; set; }
+        public string c_phone { get; set; }
+        public string c_pname { get; set; }
+        public string c_email { get; set; }
+    }
+}
---
+++ Auth/Auth/ViewModels/LloginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Auth.ViewModels
+{
+    public class LloginViewModel
+    {
+        public string txtAccount { get; set; }
+        public string txtPassword { get; set; }
+    }
+}
